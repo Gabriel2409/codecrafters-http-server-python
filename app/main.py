fa2fa47939af561715515895e6e9d3c3874558dd
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-09-02 15:48:04 +0200

    basic res

--- app/main.py
+++ app/main.py
@@ -7,8 +7,12 @@ def main():
 
     # Uncomment this to pass the first stage
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+        while True:
+            conn, address = server_socket.accept()
+            res = b"HTTP/1.1 200 OK\r\n\r\n"
+            print(res)
+            conn.sendall(res)
 
 
 if __name__ == "__main__":
